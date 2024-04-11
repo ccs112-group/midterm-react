a7ee1a6fa3aecc2614ce526b9fa8c3da5c5269fa
lorestojohngil <161902780+lorestojohngil@users.noreply.github.com>
2024-04-11 11:41:15 +0800

    Update Product.js

--- src/component/Product.js
+++ src/component/Product.js
@@ -1,12 +1,16 @@
 import React from 'react';
+import '../App.css'; // Import the CSS file
 
 const Product = ({ id, name, description, price, addToCart }) => {
   return (
-    <div>
-      <h2>{name}</h2>
-      <p>{description}</p>
-      <p>${price}</p>
-      <button onClick={() => addToCart(id)}>Add to Cart</button>
+   
+    <div className="card">
+      <div className="card-body">
+        <h2 className="card-title">{name}</h2>
+        <p className="card-text">{description}</p>
+        <p className="card-text">₱{price.toFixed(2)}</p>
+        <button className="btn btn-primary" onClick={() => addToCart(id)}>Add to Cart</button>
+      </div>
     </div>
   );
 };
