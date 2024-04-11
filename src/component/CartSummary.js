ffd901e29cfd940e87c88c7ebe86f253e305d994
jromarllegue <lleguejohnromar02@gmail.com>
2024-04-11 12:04:54 +0800

    Update CartSummary.js

--- src/component/CartSummary.js
+++ src/component/CartSummary.js
@@ -1,18 +1,38 @@
+// CartSummary.js
 import React from 'react';
+import '../App.css'; // Import the CSS file
 
 const CartSummary = ({ cart }) => {
-  // Calculate total price
-  const totalPrice = cart.reduce((total, product) => total + product.price, 0);
+  // Calculate total price and quantity
+  const totalPrice = cart.reduce((total, product) => total + product.price * product.quantity, 0);
+  const totalQuantity = cart.reduce((total, product) => total + product.quantity, 0);
 
   return (
-    <div>
-      <h2>Cart Summary</h2>
-      <ul>
-        {cart.map(product => (
-          <li key={product.id}>{product.name} - ${product.price}</li>
-        ))}
-      </ul>
-      <p>Total Price: ${totalPrice.toFixed(2)}</p>
+    <div class="cartSummary">
+      <h2 class="headerCart">Cart Summary</h2>
+      <table className="table">
+        <thead>
+          <tr>
+
+            <th>Name</th>
+            <th>Price</th>
+            <th>Quantity</th>
+            
+          </tr>
+
+        </thead>
+        <tbody>
+          {cart.map(product => (
+            <tr key={product.id}>
+              <td>{product.name}</td>
+              <td>₱{product.price.toFixed(2)}</td>
+              <td>{product.quantity}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+      <p>Total Price: ₱{totalPrice.toFixed(2)}</p>
+      <p>Total Quantity: {totalQuantity}</p>
     </div>
   );
 };
