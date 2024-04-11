384c1973948dcb11c58d5674acf4227bbb427b34
Gabrielle Lingan <161904672+LINGANGAB03@users.noreply.github.com>
2024-04-11 22:16:14 +0800

    Update ProductPages.js

--- src/component/ProductPages.js
+++ src/component/ProductPages.js
@@ -1,20 +1,26 @@
-// ProductPages.js
+ // ProductPages.js
 import React from 'react';
 import Product from './Product';
+import '../App.css'; // Import the CSS file
 
 const ProductPages = ({ products, addToCart }) => {
   return (
-    <div>
-      {products.map(product => (
-        <Product
-          key={product.id}
-          id={product.id}
-          name={product.name}
-          description={product.description}
-          price={product.price}
-          addToCart={addToCart}
-        />
-      ))}
+    <div class="cardPage">
+    <div className="container">
+      <div className="row"> 
+        {products.map(product => (
+          <div className="col-md-4" key={product.id}>
+            <Product
+              id={product.id}
+              name={product.name}
+              description={product.description}
+              price={product.price}
+              addToCart={addToCart}
+            />
+          </div>
+        ))}
+      </div>
+    </div>
     </div>
   );
 };
